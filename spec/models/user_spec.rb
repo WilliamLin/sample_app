f51595edb20796ad1380f4f91e926383c5da750d
unknown <wlin@wlin-t400.guidewire.com>
2013-03-21 22:52:03 -0400

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -49,4 +49,17 @@ describe User do
 	  end
 	  
 	end
+  require 'spec_helper'
+
+describe "User pages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_selector('h1',    text: 'Sign up') }
+    it { should have_selector('title', text: 'Sign up') }
+  end
+
 end
